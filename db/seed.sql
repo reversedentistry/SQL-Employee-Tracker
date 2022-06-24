42a3ea6ce5be05e51e995235e0b68e6194c73a8e
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-24 12:01:15 -0700

    Created dept and role table data seed

---
+++ db/seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO `department` (id, name)
+VALUES (1, "Sales"), 
+       (2, "Engineering"), 
+       (3, "Finance"), 
+       (4, "Legal"); 
+
+INSERT INTO `role` (id, title, salary, department_id)
+VALUES (1, "Sales Lead", 100000, 1),
+(2, "Salesperson", 80000, 1),
+(3, "Lead Engineer", 150000, 2),
+(4, "Software Engineer", 120000, 2),
+(5, "Account Manager", 150000, 3), 
+(6, "Accountant", 125000, 3), 
+(7, "Legal Team Lead", 200000, 4), 
+(8, "Lawyer", 190000, 4); 
+
+INSERT INTO `employee` (id, first_name, last_name, role_id, manager_id)
+VALUES (11, Tanith, Brown, 4, )
