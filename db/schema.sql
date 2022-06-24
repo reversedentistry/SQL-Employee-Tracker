88c3b146ef599a53ad9d83ff6e0c429bc44316ac
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-24 11:12:25 -0700

    Created database schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,35 @@ DROP DATABASE IF EXISTS management_db;
 CREATE DATABASE management_db; 
 
 USE management_db; 
+
+CREATE TABLE department(
+    id INT NOT NULL, 
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+); 
+
+CREATE TABLE role(
+    id INT NOT NULL, 
+    title VARCHAR(30), 
+    salary DECIMAL, 
+    department_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+); 
+
+CREATE TABLE employee(
+    id INT NOT NULL, 
+    first_name VARCHAR(30), 
+    last_name VARCHAR(30), 
+    role_id INT NOT NULL, 
+    manager_id INT NOT NULL
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL 
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+)
