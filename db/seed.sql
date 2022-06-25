41ed6f3be532714f7310496b4c83030f6c0406e6
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-24 19:14:32 -0700

    Corrected table inputs

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,10 @@
-INSERT INTO `department` (name)
-VALUES ("Sales"), 
-       ("Engineering"), 
-       ("Finance"), 
-       ("Legal"); 
+INSERT INTO `departments` (id, name)
+VALUES (1, "Sales"), 
+       (2, "Engineering"), 
+       (3, "Finance"), 
+       (4, "Legal"); 
 
-INSERT INTO `role` (id, title, salary, department_id)
+INSERT INTO `roles` (id, title, salary, department_id)
 VALUES (1, "Sales Lead", 100000, 1),
        (2, "Salesperson", 80000, 1),
        (3, "Lead Engineer", 150000, 2),
@@ -14,13 +14,13 @@ VALUES (1, "Sales Lead", 100000, 1),
        (7, "Legal Team Lead", 200000, 4), 
        (8, "Lawyer", 190000, 4); 
 
-INSERT INTO `employee` (first_name, last_name, role_id, manager_id)
-VALUES ("Tanith", "Brown", 4,  5), 
-       ("Tabitha", "Black", 5, null), 
-       ("Asuka", "Langley", 8, 7), 
-       ("Rowan", "Barry", 1, null), 
-       ("Octavia", "Tang", 3, null), 
-       ("Harper", "Han", 2, 4), 
-       ("Atticus", "Finch", 7, null), 
-       ("Kaeya", "Wisteria", 6, 2); 
+INSERT INTO `employees` (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Rowan", "Berry", 1,  null), 
+       (2, "Harper", "Han", 2, 1), 
+       (3, "Octavia", "Tang", 3, null), 
+       (4, "Tanith", "Brown", 4, 3), 
+       (5, "Tabitha", "Black", 5, null), 
+       (6, "Kaeya", "Wisteria", 6, 5), 
+       (7, "Atticus", "Finch", 7, null), 
+       (8, "Asuka", "Langley", 8, 7); 
 
