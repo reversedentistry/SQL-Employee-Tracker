ab3bd7e377f173b6b632943125c28f92067b163f
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-25 18:08:07 -0700

    Created addemployee function

--- cms.js
+++ cms.js
@@ -19,58 +19,55 @@ const updateRole = () => {
         if (err) {
             console.log(err);
         }
-        let employees = result.map(({ first_name, last_name, id }) => ({name: `${first_name} ${last_name}`, value: id}));
-            console.log(employees); 
+        let employees = result.map(({ first_name, last_name, id }) => ({ name: `${first_name} ${last_name}`, value: id }));
+        console.log(employees);
         inquirer.prompt([
             {
-                type: "list", 
+                type: "list",
                 name: "employeeUpdate",
                 message: "Which employee's role would you like to change?",
                 choices: employees
             }
         ])
-        .then(response => {
-            let employeeChoice = response.employeeUpdate; 
-            console.log(employeeChoice);
-            let params = [employeeChoice];
-            connection.query("SELECT title, role_id FROM roles", (err, result) => {
-                if (err) {
-                    console.log(err);
-                }
-                let roles = result.map(({ title, id }) => ({name: title, value: id}));
-                console.log(roles); 
-                inquirer.prompt([
-                    {
-                        type: "list", 
-                        name: "roleUpdate", 
-                        message: "What is their new role?",
-                        choices: roles
+            .then(response => {
+                let employeeChoice = response.employeeUpdate;
+                console.log(employeeChoice);
+                let params = [employeeChoice];
+                connection.query("SELECT title, role_id FROM roles", (err, result) => {
+                    if (err) {
+                        console.log(err);
                     }
-                ])
-                .then(response => {
-                    let roleChoice = (response.roleUpdate); 
-                    params.unshift(roleChoice); 
-                    connection.query("UPDATE employees SET role_id = ? WHERE id = ?", params, (err) => {
-                        if (err) {
-                            console.log(err);
-                        } 
-                        console.log("Employee updated!");
-                })
-            })
-            
-        })
-        
-    })
+                    let roles = result.map(({ title, id }) => ({ name: title, value: id }));
+                    console.log(roles);
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "roleUpdate",
+                            message: "What is their new role?",
+                            choices: roles
+                        }
+                    ])
+                        .then(response => {
+                            let roleChoice = (response.roleUpdate);
+                            params.unshift(roleChoice);
+                            connection.query("UPDATE employees SET role_id = ? WHERE id = ?", params, (err) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                console.log("Employee updated!");
+                                return viewEmployees();
+                            })
+                        })
 
-})};
+                })
+
+            })
+
+    })
+};
 
 const addEmployee = () => {
     inquirer.prompt([
-        {
-            type: "number",
-            name: "id",
-            message: "Enter the employee's ID number."
-        },
         {
             type: "input",
             name: "firstName",
@@ -82,23 +79,71 @@ const addEmployee = () => {
             message: "Enter the last name of the employee you'd like to add."
         }
     ])
-        .then(response => { connection.query("INSERT INTO employees (") })
-}
+        .then(response => {
+            let params = [response.firstName, response.lastName];
+            connection.query("SELECT title, role_id FROM roles", (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+                let roles = result.map(({ title, id }) => ({ name: title, value: id }));
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "role",
+                        message: "Choose the employee's role.",
+                        choices: roles
+                    }
+                ])
+                    .then(response => {
+                        let role = (response.role);
+                        params.push(role);
+                        connection.query("SELECT first_name, last_name, id WHERE manager_id = null", (err, result) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            let managers = result.map(({ first_name, last_name, id }) => ({ name: `${first_name} ${last_name}`, value: id }));
+                            managers.push({ name: "No manager", value: null });
+                            inquirer.prompt([
+                                {
+                                    type: "list",
+                                    name: "manager",
+                                    message: "Choose the employee's manager.",
+                                    choices: managers
+                                }
+                            ])
+                                .then(response => {
+                                    let manager = (response.manager);
+                                    params.push(manager);
+                                    connection.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", params, (err) => {
+                                        if (err) {
+                                            console.log(err);
+                                        }
+                                        console.log("Employee added");
+                                        return viewEmployees();
+                                    })
+                                })
+                        })
+                    }
+                    )
 
-const viewRoles = connection.query("SELECT * FROM roles", (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.table(result);
-})
 
-const addRole = connection.query()
+            })
+        })};
 
-const viewDepts = connection.query("SELECT * FROM departments", (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.table(result);
-})
+    const viewRoles = connection.query("SELECT * FROM roles", (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(result);
+    })
 
-const addDept = connection.query()
+    const addRole = connection.query()
+
+    const viewDepts = connection.query("SELECT * FROM departments", (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(result);
+    }); 
+
+    const addDept = connection.query()
